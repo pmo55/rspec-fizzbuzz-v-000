ea7d6912104263d9d72920853e3292aafa6facec
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-06-09 01:32:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 ==0
-  puts "Fizzbuzz"
-  elsif int % 3 == 0 
+  
+  if int % 3 == 0 
    puts "Fizz"
  elsif int % 5 == 0 
  puts "Buzz"
+  if int % 3 == 0 && int % 5 ==0
+  puts "Fizzbuzz"
  else return nil
 end
 end
